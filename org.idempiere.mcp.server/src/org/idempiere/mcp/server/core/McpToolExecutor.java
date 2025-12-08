d08c627e32b90dbc3bb201b5aece10c73ff10c90
hengsin <hengsin@gmail.com>
2025-12-09 06:27:12 +0800

    Remove the experimental oauth stuff

--- org.idempiere.mcp.server/src/org/idempiere/mcp/server/core/McpToolExecutor.java
+++ org.idempiere.mcp.server/src/org/idempiere/mcp/server/core/McpToolExecutor.java
@@ -40,7 +40,6 @@ import java.util.Base64;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 public class McpToolExecutor {
